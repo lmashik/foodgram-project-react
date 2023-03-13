e3d332da85bb888b1935146ea18c769db2f64fac
Мария Лапикова <mashik_p@mail.ru>
2023-03-13 17:01:17 +0300

    added custom permission for authors objects; removed v1 marker in urls; moved data files to backend dir

---
+++ backend/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Разрешение действия автору объета. Если не автор, то только чтение."""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
-from api.views import (
-    IngredientViewSet, RecipeViewSet, TagViewSet, CustomUserViewSet
-)
+from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
+                       TagViewSet)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -9,10 +8,10 @@ router_v1 = DefaultRouter()
 router_v1.register(r'users', CustomUserViewSet)
 router_v1.register(r'tags', TagViewSet)
 router_v1.register(r'ingredients', IngredientViewSet)
-router_v1.register(r'recipes', RecipeViewSet)
+router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-    path('v1/', include('djoser.urls')),
-    path('v1/auth/', include('djoser.urls.authtoken')),
+    path('', include(router_v1.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
