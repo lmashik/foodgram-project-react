9c3a6ac25bded7629de5ee4dd9121bcc92ee12be
Мария Лапикова <mashik_p@mail.ru>
2023-03-18 22:57:41 +0300

    fixed workflow again

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-upi#girs_v8b@02-bza2+3jdiwobrba2*h()tvz&i300t_m&l6'
+SECRET_KEY = 'django-insecure-upi#girs_v8b@02-bza2+3jdiwobrba2*h()tvz&i300t_m&l6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
