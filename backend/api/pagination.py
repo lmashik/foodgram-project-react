1b47e0ed368976b4451156c4703b0ea601ba1db3
Мария Лапикова <mashik_p@mail.ru>
2023-03-20 00:13:06 +0300

    add recipes limit parameter; changed pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,14 +2,15 @@ from rest_framework.pagination import PageNumberPagination, _positive_int
 
 
 class CustomPagination(PageNumberPagination):
-    def get_page_size(self, request):
-        page_size_query_param = request.query_params.get('limit')
-        if page_size_query_param:
-            try:
-                return _positive_int(
-                    page_size_query_param,
-                    strict=True,
-                    cutoff=self.max_page_size
-                )
-            except Exception:
-                return self.page_size
+    # def get_page_size(self, request):
+    page_size_query_param = 'limit'
+    page_size = 6
+        # if page_size_query_param:
+        #     try:
+        #         return _positive_int(
+        #             page_size_query_param,
+        #             strict=True,
+        #             cutoff=self.max_page_size
+        #         )
+        #     except Exception:
+        #         return self.page_size
