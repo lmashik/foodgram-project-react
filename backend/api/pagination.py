89bf539305a46a8b2ad1757d2ccafd29218b27c5
Мария Лапикова <mashik_p@mail.ru>
2023-03-20 00:17:30 +0300

    deleted extra comments

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,16 +1,6 @@
-from rest_framework.pagination import PageNumberPagination, _positive_int
+from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    # def get_page_size(self, request):
     page_size_query_param = 'limit'
     page_size = 6
-        # if page_size_query_param:
-        #     try:
-        #         return _positive_int(
-        #             page_size_query_param,
-        #             strict=True,
-        #             cutoff=self.max_page_size
-        #         )
-        #     except Exception:
-        #         return self.page_size
